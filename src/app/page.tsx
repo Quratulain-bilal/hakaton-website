1c64312b1ee7c34ab8bdc50d8cbc384e1f1c5238
Quratulain-bilal <umayaimanshah@gmail.com>
2024-12-18 01:55:18 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,18 +11,14 @@ import Instra from "./Components/Landingpage/Instra" ;
 import ProductDetails from "./Components/Singleproduct/ProductDetails";
 import Account from "./Components/Account/Account";
 import Blogdata from "./Components/Blogpage/Blogdata"
-
 import Checkout from "./Components/checkout/Checkout";
 
 import Shoppage from "./Components/shop/Shoppage" ;
+import Cart from "./Components/Cart"
 
 
-
-
-import Checkout from "./Components/Checkout";
 import Productinfo from "./Components/Singleproduct/Productinfo"
-import Shoppage from "./Components/shop/Shoppage" 
-
+import Contact from "./Components/Contact";
 
 
 
@@ -30,33 +26,20 @@ import Shoppage from "./Components/shop/Shoppage"
 export default function Home() {
   return (
     <div>
-
-     
-
-      
-
       <Hero />
       <GalleryImages />
       <Cards />
       <Product />
       <Blog />
       <Instra />
-
-      
-      
-
+      <Productinfo />
       <Contact />
-   
-
-
-    
+      <Cart />
       <Account />
       <Blogdata />
       <Checkout />
       <Shoppage />
       <ProductDetails />
-
-      
     </div>
   );
 }
